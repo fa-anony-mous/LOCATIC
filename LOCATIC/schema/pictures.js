5935be035d30d2dfbbfc894558e3ed90e5d0d959
fa-anony-mous <75247566+fa-anony-mous@users.noreply.github.com>
2023-03-25 18:28:52 +0530

    Delete LOCATIC directory

--- LOCATIC/schema/pictures.js
+++
@@ -1,9 +0,0 @@
-const { default: mongoose } = require("mongoose");
-
-const Pictures = new Schema({
-    name: { type: String, default: 'default' },
-    imgURL: { type: String},
-    imgID: { type: String },
-  });
-  
-mongoose.model(Pictures)
